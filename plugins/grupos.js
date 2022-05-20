6a1069cd73cd0539bd03b60c3cf0e02b08299064
Brrconejo <87559810+Brrconejo@users.noreply.github.com>
2022-05-20 14:37:19 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -6,7 +6,7 @@ let info = `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
 *➤ Grupos oficiales del Bot:*
-*1.-* https://chat.whatsapp.com/Ewx7BrjKa6B2KNMUT2vw7w
+*1.-* https://chat.whatsapp.com/HS9De4lilbmLyJluMJw0vy
 `.trim() 
 
 conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
